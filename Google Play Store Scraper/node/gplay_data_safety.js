8f47a9b44fd9ba64ecf4b5768c5b79166ad41503
EUNAH C <39269268+lunarcea@users.noreply.github.com>
2025-03-19 16:16:40 -0400

    Delete Google Play Store Scraper directory

--- Google Play Store Scraper/node/gplay_data_safety.js
+++
@@ -1,6 +0,0 @@
-var gplay = require('google-play-scraper');
-
-
-const passed_variable = process.argv[2];
-gplay.datasafety({appId: passed_variable}).then(console.log);
-
--- Google Play Store Scraper/node/gplay_permissions.js
+++
@@ -1,9 +0,0 @@
-var gplay = require('google-play-scraper');
-
-const passed_variable 	= process.argv[2];
-
-gplay.permissions({appId: passed_variable}).then((data)=>{
-	let new_data = JSON.stringify(data);
-	console.log(new_data);
-});
-
